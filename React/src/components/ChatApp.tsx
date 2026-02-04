76076dfa826edcf08356ea66d5f626fa776edf21
Mark Allen Ramirez <markallenramirez@gmail.com>
2026-02-04 20:01:59 +0800

    Add React sample

---
+++ React/src/components/ChatApp.tsx
@@ -0,0 +1,80 @@
+import { useState, useEffect, useCallback } from 'react';
+import { loadMessages } from 'devextreme/localization';
+import Chat, { FileUploaderOptions } from 'devextreme-react/chat';
+import type { ChatTypes } from 'devextreme-react/chat';
+import type { FileUploaderTypes } from 'devextreme-react/file-uploader';
+import appService from '../ChatService';
+import MessageTemplate from './MessageTemplate.tsx';
+import { CHAT_DISABLED_CLASS, user as chatUser } from '../data';
+
+export default function ChatApp(): JSX.Element {
+  const user = chatUser;
+  const [isDisabled, setDisabled] = useState(false);
+  const [typingUsers, setTypingUsers] = useState<ChatTypes.User[]>([]);
+  const [alerts, setAlerts] = useState<ChatTypes.Alert[]>([]);
+  const [chatDS, setChatDS] = useState<any>(appService.dataSource);
+  const [attachedFiles, setAttachedFiles] = useState<File[]>([]);
+
+  useEffect(() => {
+    const typingSubscription = appService.typingUsers$.subscribe(setTypingUsers);
+    const alertsSubscription = appService.alerts$.subscribe(setAlerts);
+    appService.initDataSource().then((r) => setChatDS(r));
+    return (): void => {
+      typingSubscription.unsubscribe();
+      alertsSubscription.unsubscribe();
+    };
+  }, []);
+
+  const onMessageEntered = useCallback((e: ChatTypes.MessageEnteredEvent) => {
+    setDisabled(true);
+    appService.onMessageEntered(e, setDisabled, attachedFiles);
+  }, [attachedFiles]);
+
+  const onRegenerateButtonClick = useCallback(async (): Promise<void> => {
+    setDisabled(true);
+    appService.updateLastMessage();
+
+    try {
+      await appService.regenerate();
+    } finally {
+      setDisabled(false);
+    }
+  }, []);
+
+  const messageRender = useCallback(
+    ({ message }: { message: ChatTypes.Message }) => <MessageTemplate text={message.text ?? ''} onRegenerateButtonClick={onRegenerateButtonClick} />,
+    [onRegenerateButtonClick],
+  );
+
+  const uploadFile = useCallback(() => {}, []);
+
+  const onFileUploaderValueChanged = useCallback(({ value }: FileUploaderTypes.ValueChangedEvent) => {
+    setAttachedFiles(value ?? []);
+  }, []);
+
+  return (
+    <div className="demo-container">
+      <Chat
+        className={isDisabled ? CHAT_DISABLED_CLASS : ''}
+        dataSource={chatDS}
+        reloadOnChange={false}
+        showAvatar={false}
+        height={600}
+        showDayHeaders={false}
+        user={user}
+        typingUsers={typingUsers}
+        alerts={alerts}
+        onMessageEntered={onMessageEntered}
+        messageRender={messageRender}
+      >
+        <FileUploaderOptions
+          uploadFile={uploadFile}
+          onValueChanged={onFileUploaderValueChanged}
+          uploadedMessage='File attached'
+        />
+      </Chat>
+    </div>
+  );
+}
+
+loadMessages(appService.getDictionary());
---
+++ React/src/data.ts
@@ -0,0 +1,22 @@
+const CHAT_DISABLED_CLASS = 'chat-disabled';
+const ALERT_TIMEOUT = 1000 * 60;
+const REGENERATION_TEXT = 'Regeneration...';
+const CHAT_SERVER_URL = 'http://localhost:5005/api/Chat';
+
+const assistant = {
+  id: 'assistant',
+  name: 'Virtual Assistant',
+};
+
+const user = {
+  id: 'user',
+};
+
+export {
+  CHAT_SERVER_URL,
+  REGENERATION_TEXT,
+  CHAT_DISABLED_CLASS,
+  ALERT_TIMEOUT,
+  user,
+  assistant,
+};
