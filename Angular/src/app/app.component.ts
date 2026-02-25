494c207611814233a80da705dd7e1794b9d9f8d8
Mark Allen Ramirez <markallenramirez@gmail.com>
2026-02-25 16:12:26 +0800

    Add Angular sample (#4)

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,20 +1,114 @@
-import { Component } from '@angular/core';
-import { ClickEvent } from 'devextreme/ui/button';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { type DxChatTypes } from 'devextreme-angular/ui/chat';
+import { DataSource } from 'devextreme-angular/common/data';
+import { loadMessages } from 'devextreme/localization';
+import { AppService } from './app.service';
+import { REGENERATION_TEXT } from './data';
+import { DxFileUploaderTypes } from 'devextreme-angular/ui/file-uploader';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: false
 })
-export class AppComponent {
-  title = 'Angular';
+export class AppComponent implements OnInit {
+  dataSource: DataSource | [];
 
-  counter = 0;
+  regenerationText: string = REGENERATION_TEXT;
 
-  buttonText = 'Click count: 0';
+  user: DxChatTypes.User;
 
-  onClick(_e: ClickEvent): void {
-    this.counter++;
-    this.buttonText = `Click count: ${this.counter}`;
+  typingUsers$: Observable<DxChatTypes.User[]>;
+
+  alerts$: Observable<DxChatTypes.Alert[]>;
+
+  copyButtonIcon: string;
+
+  isDisabled = false;
+
+  isDropZoneActive = false;
+
+  attachedFiles: File[] = [];
+
+  allowedFileExtensions: string[] = ['.jpg', '.jpeg', '.png'];
+
+  constructor(private readonly appService: AppService) {
+    loadMessages(this.appService.getDictionary());
+    this.user = this.appService.user;
+    this.alerts$ = this.appService.alerts$;
+    this.typingUsers$ = this.appService.typingUsers$;
+    this.copyButtonIcon = 'copy';
+    this.dataSource = [];
+
+    this.uploadFile = this.uploadFile.bind(this);
+    this.onDropZoneEnter = this.onDropZoneEnter.bind(this);
+    this.onDropZoneLeave = this.onDropZoneLeave.bind(this);
+    this.onFileUploaderValueChanged = this.onFileUploaderValueChanged.bind(this);
+  }
+
+  async ngOnInit(): Promise<void> {
+    this.dataSource = await this.appService.initDataSource() ?? [];
+  }
+
+  onFileUploaderValueChanged(e: DxFileUploaderTypes.ValueChangedEvent): void {
+    this.attachedFiles = e.value ?? [];
+  }
+
+  uploadFile() {
+    this.isDropZoneActive = false;
+  };
+
+  onDropZoneEnter({ component, dropZoneElement, event }: DxFileUploaderTypes.DropZoneEnterEvent) {
+    if (dropZoneElement.id === 'chat') {
+      const items = (event!.originalEvent as DragEvent).dataTransfer?.items ?? [];
+      const allowedFileExtensions = component.option('allowedFileExtensions') ?? [];
+      const isValidFileExtension = Array.from(items).every(i => allowedFileExtensions.includes(`.${i.type.replace(/^image\//, '')}`));
+
+      if (isValidFileExtension) {
+        this.isDropZoneActive = true;
+      }
+    }
+  }
+
+  onDropZoneLeave({ dropZoneElement }: DxFileUploaderTypes.DropZoneLeaveEvent) {
+    if (dropZoneElement.id === 'chat') {
+      this.isDropZoneActive = false;
+    }
+  }
+
+  convertToHtml(message: DxChatTypes.Message): string {
+    return this.appService.convertToHtml(message.text || '');
+  }
+
+  async onMessageEntered(e: DxChatTypes.MessageEnteredEvent): Promise<void> {
+    this.isDisabled = true;
+    try {
+      await this.appService.onMessageEntered(e, this.attachedFiles);
+    } finally {
+      this.isDisabled = false;
+    }
+  }
+
+  onCopyButtonClick(message: DxChatTypes.Message): void {
+    navigator.clipboard?.writeText(message.text ?? '').catch(() => {});
+
+    this.copyButtonIcon = 'check';
+
+    setTimeout(() => {
+      this.copyButtonIcon = 'copy';
+    }, 2500);
+  }
+
+  async onRegenerateButtonClick(): Promise<void> {
+    this.appService.updateLastMessage();
+    this.isDisabled = true;
+
+    try {
+      await this.appService.regenerate();
+    } finally {
+      this.isDisabled = false;
+    }
   }
 }
